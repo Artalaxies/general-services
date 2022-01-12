56479cd0bf1cf6d8dd5598b6ebe4d6fea4e63820
Cullen Lee <a410615903@gmail.com>
2022-01-11 16:27:43 -0800

    Initialized Home Page; Added cid rename trigger in severless function;

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,40 @@ const functions = require('firebase-functions');
 const firebaseAdmin = require('firebase-admin');
 const corsLib = require('cors');
 const metaUtil = require('@metamask/eth-sig-util');
+const multiformats = require('multiformats/cid');
+const sha2 = require('multiformats/hashes/sha2');
+const dagPB = require('@ipld/dag-pb');
+const  UnixFS = require('ipfs-unixfs');
 
-const admin = firebaseAdmin.initializeApp();
+const admin = firebaseAdmin.initializeApp({
+	storageBucket: 'art-planet.appspot.com'
+});
 const cors = corsLib({
 	origin: true,
+});
+
+
+
+exports.renameToCID = functions.storage.bucket('art-planet.appspot.com').object().onFinalize(async (object) => {
+	// console.log('listened');
+	// console.log(object);
+	const bucket = admin.storage().bucket();
+	const file = bucket.file(object.name);
+	const data = await file.download({
+		// destination: object.name,
+		validation: false
+	});
+	// console.log(data)
+	const bytes = dagPB.encode({Data: new UnixFS.UnixFS({type:'file',data: data.pop()}).marshal(),Links:[]});
+	console.log(bytes);
+	
+	const hash = await sha2.sha256.digest(bytes);
+
+	const cid = multiformats.CID.createV0(hash);
+	// console.log('cid: ' + cid.toString());
+	if(cid.toString().localeCompare(file.name)){
+		await file.rename(cid.toString());
+	}
 });
 
 
