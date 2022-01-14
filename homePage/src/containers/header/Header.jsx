ae4ce8ad3c8b51fecd75aea13b9fc214893f730b
wye-ts <jacky274865077@gmail.com>
2022-01-13 20:26:56 -0800

    update header text

--- homePage/src/containers/header/Header.jsx
+++ homePage/src/containers/header/Header.jsx
@@ -8,7 +8,7 @@ import './header.css';
 const Header = () => (
   <div className="header section__padding" id="home">
     <div className="header-content">
-      <h1 className="gradient__text">Enhance Web 3.0 with Artalaxies Platform</h1>
+      <h1 className="gradient__text">Enhance Web 3.0 with Artalaxies Platform to the multi-metaverse</h1>
       <p>Artalaxies is a decentralized marketplace that focuses on providing anonomyous art designers a platform to produce and protect their NFT derivatives with the use of web 3.0 tech stack and e-commerce system. The platform allows for buyers to purchase physical products that have the nft content embedded, without the artist loosing ownership of the NFT itself.</p>
 
       {/* <div className="header-content__input">
