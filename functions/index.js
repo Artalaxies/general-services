46ff88168416e548d6909953e09cc6cc7e9a9bd5
Cullen Lee <a410615903@gmail.com>
2022-02-05 16:13:13 -0800

    Moved out home page coding

--- functions/index.js
+++ functions/index.js
@@ -16,27 +16,28 @@ const cors = corsLib({
 
 
 
-exports.renameToCID = functions.storage.bucket('art-planet.appspot.com').object().onFinalize(async (object) => {
-	// console.log('listened');
-	// console.log(object);
-	const bucket = admin.storage().bucket();
-	const file = bucket.file(object.name);
-	const data = await file.download({
-		// destination: object.name,
-		validation: false
-	});
-	// console.log(data)
-	const bytes = dagPB.encode({Data: new UnixFS.UnixFS({type:'file',data: data.pop()}).marshal(),Links:[]});
-	console.log(bytes);
+// exports.renameToCID = functions.storage.bucket('art-planet.appspot.com').object().onFinalize(async (object) => {
+// 	// console.log('listened');
+// 	// console.log(object);
+// 	const bucket = admin.storage().bucket();
+// 	const file = bucket.file(object.name);
+// 	const data = await file.download({
+// 		// destination: object.name,
+// 		validation: false
+// 	});
+// 	// console.log(data)
+// 	const bytes = dagPB.encode({Data: new UnixFS.UnixFS({type:'file',data: data.pop()}).marshal(),Links:[]});
+// 	console.log(bytes);
 	
-	const hash = await sha2.sha256.digest(bytes);
+// 	const hash = await sha2.sha256.digest(bytes);
 
-	const cid = multiformats.CID.createV0(hash);
-	// console.log('cid: ' + cid.toString());
-	if(cid.toString().localeCompare(file.name)){
-		await file.rename(cid.toString());
-	}
-});
+// 	const cid = multiformats.CID.createV0(hash);
+// 	// console.log('cid: ' + cid.toString());
+// 	if(cid.toString().localeCompare(file.name)){
+// 		await file.rename(cid.toString());
+// 	}
+// });
+
 
 
 exports.getNonceToSign = functions.https.onRequest((request, response) =>
